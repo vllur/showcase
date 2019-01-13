9a01d6354af77e8a3b4053d8494c57e8c678f37d
vellur <veller950@gmail.com>
2019-01-13 11:20:38 +0100

    Code style & content changes

--- assets/js/toTop.js
+++ assets/js/toTop.js
@@ -2,18 +2,18 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() 
 {
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) 
-    {
-        document.getElementById("toTop").style.display = "block";
-    } 
-    else 
-    {
-        document.getElementById("toTop").style.display = "none";
-    }
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) 
+  {
+    document.getElementById("toTop").style.display = "block";
+  } 
+  else 
+  {
+    document.getElementById("toTop").style.display = "none";
+  }
 }
 
 function topFunction() 
 {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 } 
